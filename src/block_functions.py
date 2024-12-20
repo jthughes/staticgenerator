d80b8f0ba105a1bf25b866a23fc8f7c353845dda
Jeremy Hughes <jthughes@users.noreply.github.com>
2024-12-20 18:45:54 +1030

    Added block_to_block_type function

--- src/block_functions.py
+++ src/block_functions.py
@@ -2,3 +2,56 @@
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = list(filter(lambda x: len(x) > 0, map(lambda x: x.strip(), markdown.split('\n\n'))))
     return blocks
+
+def block_to_block_type(markdown_block: str) -> str:
+    def is_heading(markdown_block: str) -> bool:
+        index = 0
+        while index < len(markdown_block) and markdown_block[index] == '#':
+            index += 1
+        if (0 < index <= 6 
+            and index + 1 < len(markdown_block)
+            and markdown_block[index] == " "):
+            return True
+        return False
+    
+    def is_code(markdown_block: str) -> bool:
+        if len(markdown_block) < 7 or markdown_block[:3] != '```' or markdown_block[-3:] != '```':
+            return False
+        return True
+
+    def is_quote(markdown_block: str) -> bool:
+        lines = markdown_block.splitlines()
+        for line in lines:
+            if line.find(">") != 0:
+                return False
+        return True
+    
+    def is_unordered_list(markdown_block: str) -> bool:
+        lines = markdown_block.splitlines()
+        for line in lines:
+            if line.find("* ") != 0 and line.find("- ") != 0:
+                return False
+        return True
+
+    def is_ordered_list(markdown_block: str) -> bool:
+        lines = markdown_block.splitlines()
+        i = 1
+        for line in lines:
+            if line.find(f"{i}. ") != 0:
+                return False
+            i += 1
+        return True
+    
+    if markdown_block is None or len(markdown_block) == 0:
+        return None
+    if is_heading(markdown_block):
+        return "heading"
+    if is_code(markdown_block):
+        return "code"
+    if is_quote(markdown_block):
+        return "quote"
+    if is_unordered_list(markdown_block):
+        return "unordered_list"
+    if is_ordered_list(markdown_block):
+        return "ordered_list"
+    return "paragraph"
--- src/test_block_functions.py
+++ src/test_block_functions.py
@@ -77,5 +77,172 @@ class TestMarkdownToBlocks(unittest.TestCase):
         self.assertEqual(output[0], "# Heading")
         self.assertEqual(output[1], "body of text")
 
+class TestBlockToBlockType(unittest.TestCase):
+    def test_empty(self):
+        output = block_to_block_type("")
+        output1 = block_to_block_type(None)
+        self.assertEqual(output, None)
+        self.assertEqual(output1, None)
+
+    def test_header(self):
+        block = " Header"
+        for i in range(6):
+            block = f"#{block}"
+            output = block_to_block_type(block)
+            self.assertEqual(output, "heading")
+    
+    def test_header_no_space(self):
+        block = "Header"
+        for i in range(6):
+            block = f"#{block}"
+            output = block_to_block_type(block)
+            self.assertEqual(output, "paragraph")
+    
+    def test_header_no_space_no_text(self):
+        block = ""
+        for i in range(6):
+            block = f"#{block}"
+            output = block_to_block_type(block)
+            self.assertEqual(output, "paragraph")
+
+    def test_header_no_text(self):
+        block = "# "
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_header_level_7(self):
+        block = "####### Header"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+    
+    def test_code(self):
+        block = "```code```"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "code")
+
+    def test_code_1(self):
+        block = "```code``"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_code_2(self):
+        block = "``````code``"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+    
+    def test_code_3(self):
+        block = "``````"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_code_4(self):
+        block = "`````"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_quote_0(self):
+        block = ">"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "quote")
+
+    def test_quote_1(self):
+        block = ">\n>Hi"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "quote")
+
+    def test_quote_2(self):
+        block = ">Hi\nHow are you?\n>Good thanks"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_unordered_0(self):
+        block = "- list!"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "unordered_list")
+
+    def test_unordered_1(self):
+        block = "* list!"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "unordered_list")
+
+    def test_unordered_2(self):
+        block = "- list!\n* 2\n- 3"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "unordered_list")
+
+    def test_unordered_3(self):
+        block = "- list!\n2. 2\n- 3"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_unordered_4(self):
+        block = "-list!"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_unordered_5(self):
+        block = "- list!\n2\n- 3"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+    
+    def test_ordered_0(self):
+        block = "1. first"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "ordered_list")
+
+    def test_ordered_1(self):
+        block = "1. first\n2. second\n3. third"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "ordered_list")
+
+    def test_ordered_2(self):
+        block = "1. first\n2. second\n 3. third"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_ordered_3(self):
+        block = " 1. first\n2. second\n3. third"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_ordered_4(self):
+        block = ("1. first\n"
+                "2. second\n"
+                "3. third"
+                "4. fourth"
+                "5. fifth"
+                "6. sixth"
+                "7. seventh"
+                "8. eight"
+                "9. ninth"
+                "10. tenth")
+        output = block_to_block_type(block)
+        self.assertEqual(output, "ordered_list")
+
+    def test_ordered_5(self):
+        block = "2. first\n3. second\n4. third"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_ordered_6(self):
+        block = "0. first\n1. second\n2. third"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_ordered_7(self):
+        block = "1. first\n1. second\n3. third"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_ordered_8(self):
+        block = "1. first\nsecond\n2. third"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
+    def test_ordered_9(self):
+        block = "1. first\nsecond\n3. third"
+        output = block_to_block_type(block)
+        self.assertEqual(output, "paragraph")
+
 if __name__ == "__main__":
     unittest.main()

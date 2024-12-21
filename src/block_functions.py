1e0e2b47d358336701dcee04b8075428d4b51da8
Jeremy Hughes <jthughes@users.noreply.github.com>
2024-12-21 13:53:03 +1030

    Seemingly working markdown_to_html_node function

--- src/block_functions.py
+++ src/block_functions.py
@@ -1,3 +1,5 @@
+from nodes import *
+from inline_functions import text_to_textnodes
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = list(filter(lambda x: len(x) > 0, map(lambda x: x.strip(), markdown.split('\n\n'))))
@@ -55,3 +57,65 @@ def block_to_block_type(markdown_block: str) -> str:
     if is_ordered_list(markdown_block):
         return "ordered_list"
     return "paragraph"
+
+def markdown_to_html_node(markdown: str) -> HTMLNode:
+    def text_to_children(text: str) -> list[HTMLNode]:
+        children = []
+        nodes = text_to_textnodes(text)
+        for node in nodes:
+            children.append(text_node_to_html_node(node))
+        return children
+
+    def to_heading(block: str) -> HTMLNode:
+        level = block.find(" ")
+        node = ParentNode(f"h{level}", text_to_children(block[level + 1:]))
+        return node
+    
+    def to_code(block: str) -> HTMLNode:
+        node = ParentNode("pre",[ParentNode("code", text_to_children(block[3:-3]))])
+        return node
+
+    def to_quote(block: str) -> HTMLNode:
+        lines = block.splitlines()
+        formatted = list(map(lambda x : x[1:], lines))
+        node = ParentNode("blockquote", text_to_children(formatted))
+        return node
+
+    def to_unordered_list(block: str) -> HTMLNode:
+        lines = block.splitlines()
+        formatted = list(map(lambda x : ParentNode("li", text_to_children(x[2:])), lines))
+        node = ParentNode("ul", formatted)
+        return node
+
+    def to_ordered_list(block: str) -> HTMLNode:
+        lines = block.splitlines()
+        formatted = list(map(lambda x : ParentNode("li", text_to_children(x[2:])), lines))
+        node = ParentNode("ol", formatted)
+        return node
+
+    def to_paragraph(block: str) -> HTMLNode:
+        node = ParentNode("p", text_to_children(block))
+        return node
+
+    parent = ParentNode("div", [])
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        node_type = block_to_block_type(block)
+        node = None
+        match node_type:
+            case "heading":
+                node = to_heading(block)
+            case "code":
+                node = to_code(block)
+            case "quote":
+                node = to_quote(block)
+            case "unordered_list":
+                node = to_unordered_list(block)
+            case "ordered_list":
+                node = to_ordered_list(block)
+            case "paragraph":
+                node = to_paragraph(block)
+            case _:
+                continue
+        parent.children.append(node)
+    return parent
